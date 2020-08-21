e8dbb37459ba72fc9e2e86b1be799fb198c7bbcb
Nazif Furkan <frkndmrsy@gmail.com>
2020-08-21 03:21:57 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Mime;
+
+namespace WebUIShell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var applicationPath = AppDomain.CurrentDomain.BaseDirectory;
+
+            string UIPath = Path.Combine(applicationPath, "WebContent");
+            SimpleHttpServer server = new SimpleHttpServer(UIPath, 80);
+        }
+    }
+}
